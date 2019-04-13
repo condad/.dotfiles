9cbac42f4c5a950b76d9071ced4af226c6af3bdd
Connor Sullivan <sully4792@gmail.com>
2019-04-13 11:15:22 +0100

    FZF key bindings function call removed

--- fish_functions/fish_user_key_bindings.fish
+++ fish_functions/fish_user_key_bindings.fish
@@ -4,5 +4,3 @@ function fish_user_key_bindings
   bind -M insert -m default kj backward-char force-repaint
   bind -M visual -m default kj backward-char end-selection force-repaint
 end
-
-fzf_key_bindings
